5171ad22c0d962e6b606564e872910b89e0e2871
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-11-08 17:43:53 +0200

    Initial commit

--- task/tools/web_search.py
+++ task/tools/web_search.py
@@ -11,51 +11,50 @@ class WebSearchTool(BaseTool):
         self.__api_key = f"Bearer {open_ai_api_key}"
         self.__endpoint = "https://api.openai.com/v1/chat/completions"
 
-    # Sample of tool config:
-    # {
-    #     "type": "function",
-    #     "function": {
-    #         "name": "web_search_tool",
-    #         "description": "Tool for WEB searching.",
-    #         "parameters": {
-    #             "type": "object",
-    #             "properties": {
-    #                 "request": {
-    #                     "type": "string",
-    #                     "description": "The search query or question to search for on the web"
-    #                 }
-    #             },
-    #             "required": [
-    #                 "request"
-    #             ]
-    #         }
-    #     }
-    # }
-
     @property
     def name(self) -> str:
-        #TODO: Provide tool name as `web_search_tool`
-        raise NotImplementedError()
+        return 'web_search_tool'
 
     @property
     def description(self) -> str:
-        #TODO: Provide description of this tool
-        raise NotImplementedError()
+        return 'Makes Search in WEB by request'
 
     @property
     def input_schema(self) -> dict[str, Any]:
-        #TODO: Provide tool params Schema (it applies `request` string to search by)
-        raise NotImplementedError()
+        return {
+            "type": "function",
+            "function": {
+                "name": "web_search_tool",
+                "description": "Tool for WEB searching.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "request": {
+                            "type": "string",
+                            "description": "The search query or question to search for on the web"
+                        }
+                    },
+                    "required": [
+                        "request"
+                    ]
+                }
+            }
+        }
 
     def execute(self, arguments: dict[str, Any]) -> str:
-        #TODO:
-        # https://platform.openai.com/docs/guides/tools-web-search?api-mode=chat
-        # 1. Create `headers` dict: "Authorization": self.__api_key, "Content-Type": "application/json"
-        # 2. Create `request_data` dict with:
-        #    - "model": "gpt-4o-search-preview"
-        #    - "messages": [{"role": "user", "content": str(arguments["request"])}]
-        #    - "tools": [{"type": "static_function", "static_function": {"name": "google_search", "description": "Grounding with Google Search","configuration": {}}}]
-        #    - "temperature": 0
-        # 3. Make POST call with `requests` lib: `url=self.__endpoint, headers=headers, json=request_dat`
-        # 4. Check if response status is 200 and if yes then return message content, otherwise return `f"Error: {response.status_code} {response.text}"`
-        raise NotImplementedError()
+        headers = {
+            "Authorization": f"Bearer {self.__api_key}",
+            "Content-Type": "application/json"
+        }
+        request_data = {
+            "model": "gpt-4o-search-preview",
+            "messages": [{"role": "user", "content": str(arguments["request"])}],
+            "tools": [{"type": "static_function",
+                       "static_function": {"name": "google_search", "description": "Grounding with Google Search",
+                                           "configuration": {}}}],
+            "temperature": 0
+        }
+        response = requests.post(self.__endpoint, headers=headers, json=request_data)
+        if response.status_code == 200:
+            return response.json()[0]["message"]["content"]
+        return f"Error: {response.json()['error']}"
